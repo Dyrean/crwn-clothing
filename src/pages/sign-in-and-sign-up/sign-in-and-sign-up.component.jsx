f83041326d3f44936a683acec1c48e5e7193f43a
Fatih Canbey <fcanbeyy@gmail.com>
2021-08-31 16:39:03 +0300

    added custom button, form input and sing in components for sing-in and sing-up page

---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+import SignIn from "../../components/sign-in/sign-in.component";
+import "./sign-in-and-sign-up.styles.scss";
+
+const SignInAndSignUpPage = () => (
+  <div class="sign-in-and-sign-up"><SignIn /></div>
+);
+
+export default SignInAndSignUpPage
