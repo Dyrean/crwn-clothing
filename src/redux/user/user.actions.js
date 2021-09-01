29e51e933b0479ba8738c6a86f45a292944349d6
Fatih Canbey <fcanbeyy@gmail.com>
2021-09-01 19:12:24 +0300

    typo spelling

--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -1,4 +1,4 @@
-import { UserActionTypes } from "./user.trypes";
+import { UserActionTypes } from "./user.types";
 
 export const setCurrentUser = (user) => ({
   type: UserActionTypes.SET_CURRENT_USER,
--- src/redux/user/user.reducer.jsx
+++ src/redux/user/user.reducer.jsx
@@ -1,4 +1,4 @@
-import { UserActionTypes } from "./user.trypes";
+import { UserActionTypes } from "./user.types";
 
 const INITIAL_STATE = {
   currentUser: null,
