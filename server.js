f464843f8eef1c8b827bdce58a22c44815590f7f
Dyrean <fcanbeyy@gmail.com>
2021-09-15 22:33:11 +0300

    removed unnecessary -extra favicon

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.listen(port, (error) => {
   console.log("Server running on port " + port);
 });
 
-app.get("/service-worker.js", (req, res) => {
+app.get("/service-worker.js", (_req, res) => {
   res.sendFile(path.resolve(__dirname, "..", "build", "service-worker.js"));
 });
 
