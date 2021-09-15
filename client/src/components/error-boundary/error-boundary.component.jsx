79cee396af262744e77fbc00a3852763ba8aea90
Dyrean <fcanbeyy@gmail.com>
2021-09-16 00:18:03 +0300

    logging error

--- client/src/components/error-boundary/error-boundary.component.jsx
+++ client/src/components/error-boundary/error-boundary.component.jsx
@@ -16,6 +16,7 @@ class ErrorBoundary extends React.Component {
   }
   static getDerivedStateFromError(error) {
     // Process the error
+    console.log(error);
     return { hasErrored: true };
   }
 
