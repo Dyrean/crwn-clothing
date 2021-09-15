d673963883d06dff21a90e79c8db5602cd90d29b
Dyrean <fcanbeyy@gmail.com>
2021-09-15 23:16:35 +0300

    comprassion error ?

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
-const compression = require("compression");
-// const enforce = require("express-sslify");
+const enforce = require("express-sslify");
 
 if (process.env.NODE_ENV !== "production") require("dotenv").config();
 
@@ -15,8 +14,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 if (process.env.NODE_ENV === "production") {
-  app.use(compression());
-  // app.use(enforce.HTTPS({ trustProtoHeader: true }));
+  app.use(enforce.HTTPS({ trustProtoHeader: true }));
   app.use(express.static(path.join(__dirname, "client/build")));
 
   app.get("*", function (_req, res) {
