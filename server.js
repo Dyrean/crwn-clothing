17af3e400190c33242d1275becde53e3cd96e68f
Dyrean <fcanbeyy@gmail.com>
2021-09-16 12:51:26 +0300

    updating sagas with create cart on firestore based on userid or update

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const cors = require("cors");
 const bodyParser = require("body-parser");
 const path = require("path");
 const enforce = require("express-sslify");
@@ -13,8 +12,6 @@ const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use(cors());
 
 
 app.listen(port, (error) => {
